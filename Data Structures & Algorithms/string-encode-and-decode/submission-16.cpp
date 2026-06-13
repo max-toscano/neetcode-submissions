b02239ab86b054cd951f7904cd9d36ce25a11821
max-toscano <maxwelltoscano26@gmail.com>
2026-06-12 20:44:00 -0700

    Add: string-encode-and-decode - submission-16

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-16.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string result = ""; 
+        for (string& s : strs){
+            result += to_string(s.size());
+            result +="#";
+            result +=s; 
+        }
+        return result; 
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result; 
+        int i = 0; 
+        while (i < s.size()){ 
+            int j = i; 
+            while (s[j] != '#'){ 
+                j++;
+            };
+                int length = stoi(s.substr(i, j - i )); 
+                string str = s.substr(j + 1, length); 
+                result.push_back(str); 
+                i = j + 1 + length; 
+            }
+            return result; 
+        }
+    };
+       
+    
+ 
